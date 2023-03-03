394ca296f33ffc99f1af74f161cd3c35e386e886
YuanKe <912267428@qq.com>
2023-03-03 16:59:08 +0800

    net

--- HRNet/train_utils/train_eval_utils.py
+++ HRNet/train_utils/train_eval_utils.py
@@ -4,11 +4,11 @@ import time
 
 import torch
 
-import HRNet.transforms
+
 import HRNet.train_utils.distributed_utils as utils
 from .coco_eval import EvalCOCOMetric
 from .loss import KpLoss
-from .. import transforms
+import HRNet.transforms as transforms
 
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch,
